9987a97daf9b41c4cc73e70441f4899cb490d07e
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-12-01 17:36:47 -0300

    Feat: Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,18 @@
+import { Container } from "./styles";
+
+export function Footer() {
+  return (
+    <Container>
+      <div className="logo">
+        <svg width="44" height="48" viewBox="0 0 44 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M22.0318 0.216492L43.4349 12.0918V35.8425L22.0318 47.7179L0.628698 35.8425V12.0918L22.0318 0.216492Z" fill="#065E7C"/>
+        </svg>
+    
+        <p>food.exp</p>
+      </div>
+
+      <span>© 2022 - Todos os direitos reservados.</span>
+    </Container>
+
+  )
+}
