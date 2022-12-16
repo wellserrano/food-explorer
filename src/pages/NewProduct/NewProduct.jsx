08aeb9e60168ed4658cbf7e93b3a8284f1f3beeb
wellserrano <66437201+wellserrano@users.noreply.github.com>
2022-12-16 16:45:49 -0300

    Feat: Input file type

--- src/pages/NewProduct/NewProduct.jsx
+++ src/pages/NewProduct/NewProduct.jsx
@@ -1,12 +1,91 @@
-import { Container } from './styles'
+import { 
+  Container, 
+  Content, 
+  AddButton, 
+  ImageUpload,  
+} from './styles';
 
-import { NewProductHeader } from '../../components/NewProductHeader'
+//Hooks
+import { useState } from 'react';
+
+//Components
 import { Footer } from '../../components/Footer'
+import { TextArea } from '../../components/TextArea'
+import { TextInput } from '../../components/TextInput'
+import { MoneyInput } from '../../components/MoneyInput'
+import { TagIngredients } from '../../components/TagIngredients'
+import { NewProductHeader } from '../../components/NewProductHeader';
+
+//Icons
+import { FiUpload } from 'react-icons/fi';
+import { HiOutlineChevronLeft } from 'react-icons/hi'
 
 export function NewProduct() {
+  const [imageFile, setImageFile] = useState('');
+
+  async function handleImageUpload(event) {
+    const file = event.target.files[0];
+    setImageFile(file);
+  }
+
   return (
     <Container>
       <NewProductHeader />
+        <Content>
+          <div className='back-link'>
+            <HiOutlineChevronLeft />
+            <span>voltar</span>
+          </div>
+
+          <p className='title'>Editar prato</p>
+
+          <ImageUpload>
+            <span>Imagem do prato</span>
+
+            <label htmlFor="image">
+              <input 
+                id='image'
+                type='file'
+                onChange={ handleImageUpload }
+              />
+                <FiUpload />
+                Selecione imagem
+            </label>
+          </ImageUpload>
+
+          <div className='name-input'>
+            <TextInput 
+              caption='Nome'
+              placeholder='Ex.: Salada ceaser'
+            />
+          </div>
+
+          <div className="tags">
+            <span>Ingredientes</span>
+            <TagIngredients />
+          </div>
+
+          <div className="price">
+            <MoneyInput 
+              caption='Preço'
+              prefix='R$'
+            />
+          </div>
+
+          <div className="description">
+            <span>Descrição</span>
+            <TextArea 
+              placeholder='Fale brevemente sobre o prato, seus ingredientes e composição'
+            />
+          </div>
+
+          <div className="add-button">
+            <AddButton type='button'>
+                Adicionar pedido
+            </AddButton>
+          </div>
+
+        </Content>
       <Footer />
     </Container>
   )
--- src/pages/NewProduct/styles.js
+++ src/pages/NewProduct/styles.js
@@ -1,4 +1,186 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
 
+  display: grid;
+  grid-template-rows: 10.4rem auto 7.7rem;
+  grid-template-areas: 
+    "header"
+    "content"
+    "footer";
+
+  color: ${ ({ theme }) => theme.COLORS.WHITE };
+
+  span {
+    font-size: 1.6rem;
+    font-family: ${ ({ theme }) => theme.FONTS.ROBOTO };
+
+    color: ${ ({ theme }) => theme.COLORS.LIGHTGRAY };
+  }
+`;
+
+
+export const Content = styled.div`
+  grid-area: content;
+  
+  display: grid;
+
+  grid-template-areas: 
+    "BackLink     BackLink      BackLink"
+    "Title        Title         Title"
+    "ImageButton  NameInput     NameInput"
+    "Tags         Tags          Price"
+    "Description  Description   Description"
+    "blank        blank         AddButton";
+
+  grid-template-columns: 1fr 2fr;
+
+  
+  margin: 3rem 12rem 12rem 12rem;
+
+  > p {
+    font-size: 3.2rem;
+    margin-block: 2.4rem 3.2rem;
+  }
+
+  .back-link {
+    grid-area: BackLink;
+    
+    display: flex;
+    align-items: center;
+
+    width: 100%;
+
+    font-size: 2.4rem;
+
+    > svg {
+      font-size: 2.2rem;
+    }
+
+    > span {
+      font-family: ${ ({ theme }) => theme.FONTS.POPPINS };
+    }
+  }
+
+  .title {
+    grid-area: Title;
+    width: 100%;
+  }
+
+  .name-input {
+    grid-area: NameInput;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .tags {
+    grid-area: Tags;
+    width: 83.7rem;
+    
+    > div {
+      margin-top: .8rem;
+    }
+  }
+
+  .price {
+    grid-area: Price;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .description {
+    grid-area: Description;
+
+    margin-top: 3.2rem;
+
+    > textarea {
+      margin-top: .8rem;
+    }
+  }
+
+  .add-button {
+    grid-area: AddButton;
+    margin-top: 2.4rem;
+
+    grid-column: span 3;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+  }
+`;
+
+export const AddButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 35.7rem;
+  height: 4.8rem;
+
+  border: 1px solid white;
+  border-radius: 5px;
+  
+  background-color: rgba(255, 255, 255, 0.1);
+  color: ${ ({ theme }) => theme.COLORS.WHITE };
+
+
+`
+
+export const ImageUpload = styled.div`
+  grid-area: ImageButton;
+
+  display: flex;
+  flex-direction: column;
+  
+  width: 23rem;
+
+  label {
+    height: 4.8rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: relative;
+    
+    gap: .8rem;
+    
+    margin-top: 8px;
+    
+    border: 1px solid white;
+    border-radius: 5px;
+    
+    &:hover {
+      cursor: pointer;
+    }
+
+    > input {
+      display: none;
+
+      position: absolute;
+      top: 0; 
+      left: 0; 
+      bottom: 0; 
+      right: 0; 
+
+      background-color: transparent;
+      color: ${ ({ theme }) => theme.COLORS.WHITE };
+      
+      font-family: ${ ({ theme }) => theme.FONTS.POPPINS };
+      font-size: 1.4rem;
+  
+      > svg {
+        font-size: 2.4rem;
+      }
+    }
+  }
+
+`;
