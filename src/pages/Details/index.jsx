ab96d64650dbbc5770bbbb214226af3e11c92438
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-10 12:44:31 -0300

    Details page created
    Co-authored-by: Luxis <0Luxis0@users.noreply.github.com>

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,16 @@
+import { Container, Content } from './styles'
+
+//Components	
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
+
+
+export function Details() {
+  return (
+    <Container>
+      <Header />
+      <Footer />  
+    </Container>
+  )
+}
+
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home'
+import { Details } from '../pages/Details'
 import { NewProduct } from '../pages/NewProduct';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={ <Home /> } />
+      <Route path="/details" element={ <Details />} />
       <Route path="/newproduct" element={ <NewProduct />} />
     </Routes>
   )
