a3e4731b9ee60b475ae4b63b2767cdea1ce74e61
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-10 15:36:18 -0300

    Back button and Food title
    Co-authored-by: Luxis <0Luxis0@users.noreply.github.com>

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,33 @@
-import { Container, Content } from './styles'
+import { Container, ContentLeft, ContentRight } from './styles'
 
 //Components	
 import { Header } from '../../components/Header'
 import { Footer } from '../../components/Footer'
 
+//Icons
+import { HiOutlineChevronLeft } from 'react-icons/hi'
+
 
 export function Details() {
   return (
     <Container>
+      
       <Header />
+
+        <ContentLeft>
+          <div className='back-link'>
+            <HiOutlineChevronLeft />
+            <span>voltar</span>
+          </div>
+        </ContentLeft>
+
+        <ContentRight>
+          <h2>Salada Ravanello</h2>
+          <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim</p>
+        </ContentRight>
+
       <Footer />  
+
     </Container>
   )
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -9,10 +9,50 @@ export const Container = styled.div`
 
   display: grid;
   grid-template-rows: 10.4rem auto 7.7rem;
-  grid-template-areas:
-  "header       header"
-  "contentLeft  contentRight"
-  "footer       footer";
+  grid-template-areas: 
+    "header       header" 
+    "contentLeft  contentRight" 
+    "footer       footer";
   `;
 
-export const Content = styled.div``;
+export const ContentLeft = styled.div`
+  grid-area: contentLeft;
+
+  color: ${ ({ theme }) => theme.COLORS.WHITE };
+
+  /* background-color: white; */
+
+  .back-link {
+    grid-area: BackLink;
+    
+    display: flex;
+    align-items: center;
+
+    width: 100%;
+
+    font-size: 2.4rem;
+
+    > svg {
+      font-size: 3.2rem;
+    };
+
+    > span {
+      font-family: ${ ({ theme }) => theme.FONTS.POPPINS };
+      font-weight: 100;
+    }
+
+  };
+`;
+
+export const ContentRight = styled.div`
+  grid-area: contentRight;
+
+  > h2 {
+    color: ${ ({ theme }) => theme.COLORS.WHITE };
+  }
+
+  > p {
+    color: ${ ({ theme }) => theme.COLORS.WHITE2 };;
+  }
+
+`;
