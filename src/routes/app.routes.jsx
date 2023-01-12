85d0cb1e00069e11f43e8629f238a8ba51e1e777
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-12 11:35:05 -0300

    ft: Orders page created

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home'
+import { Orders } from '../pages/Orders'
 import { Details } from '../pages/Details'
 import { Checkout } from '../pages/Checkout'
 import { Favorites } from '../pages/Favorites'
@@ -10,6 +11,7 @@ export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={ <Home /> } />
+      <Route path="/orders" element={ <Orders />} />
       <Route path="/details" element={ <Details />} />
       <Route path="/checkout" element={ <Checkout /> } />
       <Route path="/favorites" element={ <Favorites /> } />
