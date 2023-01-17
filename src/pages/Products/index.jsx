1ca03da901da0625d50d6eaa02acb06402af9c07
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-17 14:56:59 -0300

    fix: function parameters useEffect

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -24,7 +24,7 @@ export function Products() {
   const params = useParams();
 
   useEffect(() => {
-    async function fetchProductDetails(req, res) {
+    async function fetchProductDetails() {
       
       const response = await api.get(`/products?product_id=${params.product_id}`)
 
