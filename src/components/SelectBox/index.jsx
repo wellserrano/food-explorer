c13559d783082ad24dafdd6a0a3157806c52b400
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-01-31 19:12:51 -0300

    feat: changing status updates on database

--- src/components/SelectBox/index.jsx
+++ src/components/SelectBox/index.jsx
@@ -1,43 +1,56 @@
 import { Container } from "./styles";
 
-import { BsChevronDown } from 'react-icons/bs'
-
 import Select from 'react-select'
+import { useState } from "react";
 
+import { api } from "../../services/api";
+import { useAuth } from "../../hooks/auth";
 
+const customStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    backgroundColor:'#010A0F',
+  }),
+  
+  option: (provided, state) => ({
+    ...provided,
+    color: `${ ({ theme }) => theme.COLORS.WHITE2 }`,
+    backgroundColor:'#010A0F',
+  }),
 
-export function SelectBox({ status }) {
+  singleValue: (provided, state) => ({
+    ...provided,
+    color: `${ ({ theme }) => theme.COLORS.WHITE1 }`
+  })
+
+};
+
+export function SelectBox({ status, order }) {
+  const [selectedOption, setSelectedOption] = useState({ value: status, label: status });
   const options = [
-    { value: 'pendente', label: 'Pendente' },
-    { value: 'preparando', label: 'Preparando' },
-    { value: 'entregue', label: 'Entregue' }
+    { value: 'Pendente', label: 'Pendente' },
+    { value: 'Preparando', label: 'Preparando' },
+    { value: 'Entregue', label: 'Entregue' }
   ]
 
-  const customStyles = {
-    control: (provided, state) => ({
-      ...provided,
-      backgroundColor:'#010A0F',
-    }),
+  const { user } = useAuth();
+  const isAdmin = user.admin;
+
+  async function handleStatusChange(newOption) {
+    setSelectedOption(newOption)
     
-    option: (provided, state) => ({
-      ...provided,
-      color: `${ ({ theme }) => theme.COLORS.WHITE2 }`,
-      backgroundColor:'#010A0F',
-    }),
-
-    singleValue: (provided, state) => ({
-      ...provided,
-      color: `${ ({ theme }) => theme.COLORS.WHITE1 }`
-    })
-
-  };
+    await api.put(`/orders?order_id=${order}&status=${newOption.value}`)
+  }
 
   return (
     <Container>
       <Select 
-        options={ options }
+        value={ selectedOption }
         styles={ customStyles }
-        placeholder={ status }
+        options={ options }
+        placeholder={ selectedOption }
+        onChange={ handleStatusChange }
+        isDisabled={ !isAdmin }
       />
     </Container>
   )
