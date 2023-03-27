ffcabc18a7d0faf43698c44abd7cd25ff347b94a
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-03-27 15:34:31 -0300

    style: react importation and semi collons

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import { Container } from './styles'
 
-export function SearchInput({ icon: Icon, handle, ...rest }) {
+export function SearchInput ({ icon: Icon, handle, ...rest }) {
   return (
     <Container { ...rest }>
       { Icon && <Icon onClick={ handle }/> }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { AuthProvider } from './hooks/auth'
@@ -6,22 +6,21 @@ import { AuthProvider } from './hooks/auth'
 import { Routes } from './routes'
 
 import GlobalStyle from './styles/global'
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components'
 import { darkTheme, lightTheme } from './styles/theme'
 
-import { useState } from 'react'
 import { ThemeContext } from './hooks/theme'
 
-function App() {
-  const [theme, setTheme] = useState(darkTheme);
-  
-  function toggleSwitch() {
-    const actualTheme = theme === darkTheme ? lightTheme : darkTheme;
+function App () {
+  const [theme, setTheme] = useState(darkTheme)
+
+  function toggleSwitch () {
+    const actualTheme = theme === darkTheme ? lightTheme : darkTheme
     setTheme(actualTheme)
   }
 
   return (
-    <ThemeContext.Provider value={{theme, toggleSwitch}}>
+    <ThemeContext.Provider value={{ theme, toggleSwitch }}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <AuthProvider>
@@ -29,15 +28,15 @@ function App() {
         </AuthProvider>
       </ThemeProvider>
     </ThemeContext.Provider>
-  );
+  )
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App styles={{
-      "@media (max-width: 480px)": {
+      '@media (max-width: 480px)': {
         backgroundColor: 'red'
       }
     }}/>
   </React.StrictMode>
-);
+)
